f822cdaa942dbb49f105d20656a4da8b1ee7faa3
shouvick bachhar <bachharshouvickp2@gmail.com>
2025-03-17 23:09:10 +0530

    mixed file added

---
+++ app/src/main/java/com/example/gitpractice/firstAndSecondMix.kt
@@ -0,0 +1,15 @@
+package com.example.gitpractice
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun MixedCode(){
+    Column() {
+        Text(
+            text = "first person 1"
+        )
+    }
+}
